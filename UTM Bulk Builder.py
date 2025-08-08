2c6172a4c39951a5a2da15150e2114ae7a9667f9
hexabenja <benjamin.n.ign@gmail.com>
2025-08-08 00:19:26 -0400

    Se agregó documentación en las librerias utilizadas, definiciones de URL, nueva función agregada 'filtered_utm_params', juntar url y queries y definición de main

--- UTM Bulk Builder.py
+++ UTM Bulk Builder.py
@@ -3,29 +3,30 @@
 
 # In[1]:
 
-
+#Librerias para descomponer juntar y reconocer URLs
 from urllib.parse import urlencode, urlparse, urlunparse, parse_qsl
 
    
-
+#Define generate_utm_url como los valores de base_url y utm_params
 def generate_utm_url(base_url, utm_params):
-    parsed_url = urlparse(base_url)
-    query_params = dict(parse_qsl(parsed_url.query))
+    parsed_url = urlparse(base_url) #Desarma URL base (subdominio, dominio, slash"
+    query_params = dict(parse_qsl(parsed_url.query)) #Desarma las queries de la URL (Representadas por ? en BBDD SQL)
 
     # NUEVO: Filtrar solo parámetros UTM que no estén vacíos
     filtered_utm_params = {k: v for k, v in utm_params.items() if v.strip()}
+    #K es la Key, V es el valor y v.strip verifica si es que hay espacios y si hay contenido al final en V
 
-    query_params.update(filtered_utm_params) #Se reemplaza por valor NUEVO
-    updated_query = urlencode(query_params)
-    updated_url = urlunparse(parsed_url._replace(query=updated_query))
+    query_params.update(filtered_utm_params) #Se reemplaza por valor NUEVO de utm_params a los nuevos filtrados por la función de arriba
+    updated_query = urlencode(query_params) #Juntar los queries (UTM)
+    updated_url = urlunparse(parsed_url._replace(query=updated_query)) #URL armada devuelta y reemplazada por updated_query
     return updated_url
 
-def main():
-    urls = []
-    while True:
-        url = input("Enter website URL (or 'done' to finish): ")
-        if url.lower() == "done":
-            break #Detener sección URLS
+def main(): #Definir programa main
+    urls = [] #Función urls
+    while True: #Mientras sea cierto "urls"
+        url = input("Enter website URL (or 'done' to finish): ") # El valor URL será el input ingresado
+        if url.lower() == "done": #En caso de que el input de url.lower sea "done" 
+            break #Se detiene la función url del input 
         urls.append(url)
 
     utm_params = {
